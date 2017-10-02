00952120363515eb55e897a462434dd255b4fc04
Quentin Stafford-Fraser <quentin@pobox.com>
2017-10-02 21:01:51 +0100

    1.0.0 version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-major_version = 0
-minor_version = 9
+major_version = 1
+minor_version = 0
 build_version = 0
 
 version = str(major_version) + '.' + str(minor_version) + '.' + str(build_version)
