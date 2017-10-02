318ee730270b5e6ddbeea2c67807816ec54325aa
Quentin Stafford-Fraser <quentin@pobox.com>
2017-10-02 20:56:32 +0100

    Getting closer to PyPI

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+major_version = 0
+minor_version = 9
+build_version = 0
+
+version = str(major_version) + '.' + str(minor_version) + '.' + str(build_version)
+
+setup(
+    name='redmine-tidy',
+    version=version,
+    description='Redmine Tidy: Close tickets with no recent updates',
+    author='Quentin Stafford-Fraser',
+    url='https://github.com/telemarq/redmine_tidy',
+    license='GNU GPL 2',
+    packages=(),
+    scripts = ['redmine_tidy.py'],
+    install_requires=('python-redmine','docopt'),
+)
